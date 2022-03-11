82027a9352f788101b0c2a853eab2dea1c48bf28
Alexandre Machado <alexmm1996@gmail.com>
2022-03-11 11:07:20 +0000

    AddArticle + Register

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Articles from "./articles/Articles";
 import './App.css'
 import Login, {ProtectedRoute} from "./Login/Login";
 import Register from "./Register/register";
+import AddArticle from "./Components/AddArticle";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Link to="/articles">News</Link>
       <Link to="/Login">Login</Link>
       <Link to="/Register">Register</Link>
+      <Link to="/AddArticle">AddArticle</Link>
     </nav>
 
       <Routes>
@@ -21,6 +23,7 @@ function App() {
         <Route path="*" element={() => <p>Page Not Found</p>} />
         <Route exact={true} path="/Login" element={<Login/>}/>
         <Route exact={true} path="/Register" element={<Register/>}/>
+        <Route exact={true} path="/AddArticle" element={<AddArticle/>}/>
       </Routes>
     </>
 );
